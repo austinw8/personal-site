6ec8602acc4eb737c0ab448cf917b76961354529
Austin Mangelson <austin.mangelson@legrandehealth.com>
2025-11-01 16:27:04 -0600

    update package dependencies and migrate to Vite

---
+++ src/data/techStack.js
@@ -0,0 +1,146 @@
+// Tech Stack Configuration
+
+const TECH_STACK = {
+	languages: {
+		title: "Languages",
+		items: [
+			{
+				name: "TypeScript",
+				logo: "/logos/typescript_logo.png",
+				enabled: true,
+			},
+			{
+				name: "Python",
+				logo: "/logos/python_logo.png",
+				enabled: true,
+			},
+			{
+				name: "JavaScript",
+				logo: "/logos/javascript_logo.png",
+				enabled: true,
+			},
+			{
+				name: "SQL",
+				logo: "/logos/sql_logo.png",
+				enabled: true,
+			},
+			{
+				name: "R",
+				logo: "/logos/r_logo.png",
+				enabled: true,
+			},
+			{
+				name: "HTML",
+				logo: "/logos/html_logo.png",
+				enabled: true,
+			},
+			{
+				name: "CSS",
+				logo: "/logos/css_logo.png",
+				enabled: true,
+			},
+		],
+	},
+	frameworks: {
+		title: "Frameworks & Libraries",
+		items: [
+			{
+				name: "React",
+				logo: "/logos/react_logo.png",
+				enabled: true,
+			},
+			{
+				name: "Next.js",
+				logo: "/logos/nextjs_logo.png",
+				enabled: false,
+			},
+			{
+				name: "FastAPI",
+				logo: "/logos/fastapi_logo.png",
+				enabled: false,
+			},
+			{
+				name: "Node.js",
+				logo: "/logos/nodejs_logo.png",
+				enabled: false,
+			},
+			{
+				name: "Tailwind",
+				logo: "/logos/tailwind_logo.png",
+				enabled: true,
+			},
+			{
+				name: "SQLAlchemy",
+				logo: "/logos/sqlalchemy_logo.png",
+				enabled: false,
+			},
+			{
+				name: "Streamlit",
+				logo: "/logos/streamlit_logo.png",
+				enabled: true,
+			},
+		],
+	},
+	databases: {
+		title: "Databases",
+		items: [
+			{
+				name: "PostgreSQL",
+				logo: "/logos/postgres_logo.png",
+				enabled: true,
+			},
+			{
+				name: "MongoDB",
+				logo: "/logos/mongobd_logo.png",
+				enabled: false,
+			},
+			{
+				name: "Firebase",
+				logo: "/logos/firebase_logo.png",
+				enabled: false,
+			},
+			{
+				name: "BigQuery",
+				logo: "/logos/bigquery_logo.png",
+				enabled: true,
+			},
+		],
+	},
+	tools: {
+		title: "Tools & Platforms",
+		items: [
+			{
+				name: "Docker",
+				logo: "/logos/docker_logo.png",
+				enabled: true,
+			},
+			{
+				name: "Git",
+				logo: "/logos/git_logo.png",
+				enabled: true,
+			},
+			{
+				name: "GCP",
+				logo: "/logos/gcp_logo.png",
+				enabled: true,
+			},
+			{
+				name: "DBeaver",
+				logo: "/logos/dbeaver_logo.png",
+				enabled: true,
+			},
+			{
+				name: "Looker Studio",
+				logo: "/logos/looker_studio_logo.png",
+				enabled: true,
+			},
+			{
+				name: "Meltano",
+				logo: "/logos/meltano_logo.png",
+				enabled: true,
+			},
+		],
+	},
+};
+
+export default TECH_STACK;
